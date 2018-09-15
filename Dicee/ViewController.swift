2a3aadfb0b13bff3d29f27e4716a3549dff7b945
Jonas Raphael Schultheiss <dev.prsm@gmail.com>
2018-09-15 16:05:30 +0200

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Jonas Raphael Schultheiss on 15.09.18.
+//  Copyright © 2018 Jonas Raphael Schultheiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
